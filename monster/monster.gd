0054860f10e9b46ee18afc26d03550f8427e48fa
Yassin Ch <sinoo.sy3@gmail.com>
2025-04-22 20:19:54 +0200

    Update Monster location

--- monster/monster.gd
+++ monster/monster.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
-class_name Monster
-
-@export var speed = 300
-@onready var player = $"../Player"
+@export var speed = 15000
 @onready var sprite = $AnimatedSprite2D
 
-func _physics_process(_delta):
+var is_chasing_player: bool = false
+var target
+
+func _physics_process(delta: float) -> void:
 	
 	# Handle animations based on direction
-	# var direction = global_position.direction_to(player.global_position)
-	# velocity = direction * speed
+	#var direction = global_position.direction_to(player.global_position)
+	#velocity = direction * speed
 	
 	## Simple animation - just flip sprite based on x direction
 	#if direction.x > 0:
@@ -18,13 +18,15 @@ func _physics_process(_delta):
 	#elif direction.x < 0:
 		#sprite.flip_h = true   # Face left
 	
-	move_and_slide()
-
+	if is_chasing_player:
+		sprite.play("run")
+		move_and_slide()
+		var direction = global_position.direction_to(target.global_position)
+		velocity = direction * speed * delta
+	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
-		chase_player()
-
-func chase_player() -> void:
-	sprite.play("run")
-	move_and_slide()
+	if body.is_in_group("player"):
+		print("Caught the player")
+		is_chasing_player = true
+		target = body
