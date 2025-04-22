f564593ccf15696bbc65ca088bbbc12058db7b54
Omar <hydrah@gmx.com>
2025-04-22 15:08:05 -0300

    Remove second collision shape

--- monster.gd
+++ monster.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-@export var speed = 300
+@export var speed = 15000
 @onready var sprite = $AnimatedSprite2D
 
-func _physics_process(_delta):
+var is_chasing_player: bool = false
+var target
+
+func _physics_process(delta: float) -> void:
 	
 	# Handle animations based on direction
 	#var direction = global_position.direction_to(player.global_position)
@@ -15,14 +18,16 @@ func _physics_process(_delta):
 	#elif direction.x < 0:
 		#sprite.flip_h = true   # Face left
 	
-	# Play run animation (you need a "run" animation in your AnimatedSprite2D)
-	sprite.play("run")
+	if is_chasing_player:
+		sprite.play("run")
+		move_and_slide()
+		var direction = global_position.direction_to(target.global_position)
+		velocity = direction * speed * delta
 	
-	# Smooth movement
-	move_and_slide()
-	
-
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("Caught the player")
+		is_chasing_player = true
+		target = body
+		
